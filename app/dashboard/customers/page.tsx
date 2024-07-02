1c2a00dd2ed42ed839a18b14c739d00c32edadd2
KirtiYadav1681 <yadavkirti7745@gmail.com>
2024-07-02 16:43:39 +0530

    updated users section

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,5 @@
 import React,{Suspense} from "react";
-import Pagination from '@/app/ui/invoices/pagination';
-import { fetchCustomerPages, fetchFilteredCustomers } from "@/app/lib/data";
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
+import { fetchFilteredCustomers } from "@/app/lib/data";
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import Table from '../../ui/customers/table';
 
@@ -11,21 +8,15 @@ export default async function Page({
 }: {
   searchParams?: {
     query?: string;
-    page?: string;
   };
 }) {
   const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
 
   const customers = await fetchFilteredCustomers(query);
-  const totalPages = await fetchCustomerPages(query);
 
     return <div className="w-full">
      <Suspense fallback={<InvoicesTableSkeleton />}>
       <Table customers={customers} />
     </Suspense>
-    <div className="mt-5 flex w-full justify-center">
-      <Pagination totalPages={totalPages} />
-    </div>
   </div>;
   }
